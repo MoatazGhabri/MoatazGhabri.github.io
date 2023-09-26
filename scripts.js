a90cded3085d3351da08d8b50649185246420e7e
MoatazGhabri <120782597+MoatazGhabri@users.noreply.github.com>
2023-09-26 09:51:20 +0100

    new

--- scripts.js
+++ scripts.js
@@ -96,3 +96,24 @@ menuIcon.addEventListener('click', () => {
         menuOpen = false;
     }
 });
+window.addEventListener('load', () => {
+    // Hide the loading spinner when the page finishes loading
+    const loadinglogo = document.querySelector('.loading-logo');
+    
+    // Increase the transition delay to 2000 milliseconds (2 seconds)
+    loadinglogo.style.transition = 'opacity 20s';
+
+    // Fade out
+    loadinglogo.style.opacity = '0';
+
+    setTimeout(() => {
+        loadinglogo.style.display = 'none'; // Hide
+    }, 20000); // Delay for the fade-out transition (adjust as needed)
+});
+
+// Show the jumping logo when the page is refreshing
+window.addEventListener('beforeunload', () => {
+    document.querySelector('.loading-logo').style.display = 'flex';
+});
+
+
